31d21ee2f031096fb90669e65aebcc5e415160af
karolnowak98 <karol.nowak2298@gmail.com>
2021-05-25 15:17:58 +0200

    RecyclerView for tasks - #4

--- app/src/main/java/com/example/taskmanagement/AddTaskActivity.java
+++ app/src/main/java/com/example/taskmanagement/AddTaskActivity.java
@@ -2,47 +2,125 @@ package com.example.taskmanagement;
 
 import android.app.AlertDialog;
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 public class AddTaskActivity extends AppCompatActivity {
 
     private DatePickerDialog datePickerDialog;
-    private Button btnTaskDate, btnAddTask, btnCancel;
+    private Button btnTaskDate;
     private EditText etTaskName;
     private TextView tvTaskName;
+    private RadioGroup rgTasks;
+    private boolean test = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
+
         initDatePicker();
 
         btnTaskDate = findViewById(R.id.btn_picker_task_date);
-        btnAddTask = findViewById(R.id.btn_add_task);
-        btnCancel = findViewById(R.id.btn_cancel);
+        Button btnAddTask = findViewById(R.id.btn_add_task);
+        Button btnCancel = findViewById(R.id.btn_cancel);
+        etTaskName = findViewById(R.id.et_task_name);
+        tvTaskName = findViewById(R.id.tv_task_name);
+        rgTasks = findViewById(R.id.rg_tasks);
 
-        btnTaskDate.setText(getTodaysDate());
+        if(test)
+            btnTaskDate.setText(getTodaysDate());
+
         btnAddTask.setBackgroundColor(getResources().getColor(R.color.btn_green));
         btnCancel.setBackgroundColor(getResources().getColor(R.color.btn_red));
 
-        btnCancel.setOnClickListener(v -> {
-            Intent intent = new Intent();
-            setResult(RESULT_OK, intent);
-            finish();
+        btnAddTask.setOnClickListener(v -> {
+            Intent intent = new Intent(this, MainActivity.class);
+            if(validateNewTask()) {
+                String taskCategory = ((RadioButton) findViewById(rgTasks.getCheckedRadioButtonId())).getText().toString();
+                intent.putExtra("taskName", etTaskName.getText().toString());
+                intent.putExtra("taskCategory", taskCategory);
+                intent.putExtra("taskDate", btnTaskDate.getText().toString());
+                MainActivity.NUMBER_OF_TASKS++;
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+            else{
+                //setResult(RESULT_OK, intent);
+                //finish();
+            }
         });
 
-
+        btnCancel.setOnClickListener(v -> {
+            Intent intent = new Intent();
+            setResult(RESULT_CANCELED, intent);
+            finish();
+        });
     }
+
+    private void showDateTimeDialog(final Button btnTaskDate) {
+        final Calendar calendar = Calendar.getInstance();
+        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                calendar.set(Calendar.YEAR,year);
+                calendar.set(Calendar.MONTH,month);
+                calendar.set(Calendar.DAY_OF_MONTH,dayOfMonth);
+
+                TimePickerDialog.OnTimeSetListener timeSetListener = new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        calendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                        calendar.set(Calendar.MINUTE,minute);
+
+                        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yy-MM-dd HH:mm");
+
+                        btnTaskDate.setText(simpleDateFormat.format(calendar));
+                    }
+                };
+                new TimePickerDialog(AddTaskActivity.this, timeSetListener, calendar.get(Calendar.HOUR_OF_DAY),calendar.get(Calendar.MINUTE),false).show();
+            }
+        };
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+
+        outState.putInt("YEAR", datePickerDialog.getDatePicker().getYear());
+        outState.putInt("MONTH", datePickerDialog.getDatePicker().getMonth());
+        outState.putInt("DAY", datePickerDialog.getDatePicker().getDayOfMonth());
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        test = false;
+        super.onRestoreInstanceState(savedInstanceState);
+        datePickerDialog.getDatePicker().updateDate(savedInstanceState.getInt("YEAR"),
+                                                    savedInstanceState.getInt("MONTH"),
+                                                    savedInstanceState.getInt("DAY"));
+    }
+
+    //______________________DATE METHODS______________________
 
     private String getTodaysDate() {
         Calendar cal = Calendar.getInstance();
@@ -54,14 +132,10 @@ public class AddTaskActivity extends AppCompatActivity {
     }
 
     private void initDatePicker(){
-        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
-                month += 1;
-                String date = makeDateString(day, month, year);
-                btnTaskDate.setText(date);
-
-            }
+        DatePickerDialog.OnDateSetListener dateSetListener = (datePicker, year, month, day) -> {
+            month += 1;
+            String date = makeDateString(day, month, year);
+            btnTaskDate.setText(date);
         };
 
         Calendar cal = Calendar.getInstance();
@@ -72,6 +146,7 @@ public class AddTaskActivity extends AppCompatActivity {
         int style = AlertDialog.THEME_HOLO_LIGHT;
 
         datePickerDialog = new DatePickerDialog(this, style, dateSetListener, year, month, day);
+        datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis()-10000);
     }
 
     private String makeDateString(int day, int month, int year) {
@@ -106,11 +181,13 @@ public class AddTaskActivity extends AppCompatActivity {
         return "stycznia";
     }
 
-    private boolean validateNewTask(){
-        return true;
-    }
-
     public void openDatePicker(View view) {
         datePickerDialog.show();
+    }
+
+    private boolean validateNewTask(){
+        if ((etTaskName.getText().toString().trim().equals("")) || (rgTasks.getCheckedRadioButtonId() == -1))
+            return false;
+        return true;
     }
 }
--- app/src/main/java/com/example/taskmanagement/MainActivity.java
+++ app/src/main/java/com/example/taskmanagement/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.taskmanagement;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.graphics.Color;
@@ -10,22 +13,60 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
+    public static int NUMBER_OF_TASKS = 0;
+    private static final int REQUEST_CODE = 1;
 
+    private ArrayList<TaskModel> mData;
+    private RecyclerView mListOfTasks;
+    private String taskName, taskCategory, taskDate;
+    private TextView tv_no_tasks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mData = new ArrayList<TaskModel>();
+        tv_no_tasks = findViewById(R.id.tv_no_tasks);
+
+        if(savedInstanceState != null){
+            mData = savedInstanceState.getParcelableArrayList("COS");
+        } else{
+            for(int i = 0; i < NUMBER_OF_TASKS; i++){
+                mData.add(new TaskModel(taskName, taskCategory, taskDate));
+            }
+        }
+
 
         Button btnAddTask = findViewById(R.id.btn_add_task);
         btnAddTask.setBackgroundColor(getResources().getColor(R.color.btn_green));
         btnAddTask.setOnClickListener(v -> {
-            Intent intent = new Intent(getApplicationContext(), AddTaskActivity.class);
-            startActivity(intent);
+            Intent intentAddTask = new Intent(getApplicationContext(), AddTaskActivity.class);
+            startActivityForResult(intentAddTask, REQUEST_CODE);
         });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent newData) {
+        super.onActivityResult(requestCode, resultCode, newData);
+        if(requestCode == REQUEST_CODE && resultCode == RESULT_OK){
+            if(NUMBER_OF_TASKS!=0)
+                tv_no_tasks.setVisibility(View.GONE);
 
+            Bundle bundle = newData.getExtras();
+            taskName = bundle.getString("taskName");
+            taskCategory = bundle.getString("taskCategory");
+            taskDate = bundle.getString("taskDate");
+
+            mData.add(new TaskModel(taskName, taskCategory, taskDate));
+            InteractiveArrayAdapter adapter = new InteractiveArrayAdapter(this, mData);
+
+            mListOfTasks = findViewById(R.id.rv_list_of_tasks);
+            mListOfTasks.setAdapter(adapter);
+            mListOfTasks.setLayoutManager(new LinearLayoutManager(this));
+        }
+    }
 }
